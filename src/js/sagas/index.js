72ceed03e3a0993ecf44e59538350a3377dbd221
vamship <vamshi.ponnapalli@gmail.com>
2016-10-11 13:48:34 -0400

    Renamed saga files

--- src/js/sagas/index.js
+++ src/js/sagas/index.js
@@ -1,8 +1,8 @@
 import { fork } from 'redux-saga/effects';
 import userSagas from './user-sagas';
 import navSagas from './nav-sagas';
-import assetListSagas from './asset-list-sagas';
-import cameraListSagas from './camera-list-sagas';
+import assetListSagas from './asset-sagas';
+import cameraListSagas from './camera-sagas';
 
 export default function*() {
     yield[
